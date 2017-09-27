7c6f9f8587a202b0451e42307db187efa92c2aac
kurtgee6 <kurtgee11@gmail.com>
2017-09-27 15:51:40 -0400

    information is appened on articles div

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,24 @@
 $("#button").click(function () {
     $("#button").fadeOut("slow");
     $("#instructions").fadeOut("slow");
+    $("#articles").fadeIn("slow");
 
     $.getJSON("/articles", function (data) {
-        console.log(data);
+
+        if (typeof data !== 'undefined' && data.length === 0) {
+            $("#articles").append("<h1>" + "Sorry, failed to grab data. Please go back and try again. Please click the running man" + "</h1>")
+        } else {
+
+            Materialize.toast('To grab more articles, press on the running Man! ', 3000)
+
+            for (var i = 0; i < data.length; i++) {
+                $('h1').empty();
+
+                // Display the information on the page
+                $("#articles").append("<p data-id='" + data[i].headline + "'>" + data[i].summary + "<br />" + data[i].author + "</p>");
+            }
+
+        }
     });
 
 });
